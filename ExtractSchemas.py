9dfa9037fe39cbe85630188406bc5951597389a7
Fatos Ismali <fatos.ismajli@gmail.com>
2015-10-24 16:48:00 +0100

    Included code to recursively go through all the subdirectories of a directory and read all the files therein.

--- ExtractSchemas.py
+++ ExtractSchemas.py
@@ -1,19 +1,28 @@
 __author__ = 'fatosismali'
 
 import xlrd
-import os
+import os, fnmatch
 from os import listdir
 from os.path import isfile, join
 
 
-mypath = "/Users/fatosismali/Desktop/DataScience/MinistryOfData/datasets/Kosovo datasets/THV_owns source revenues/"
-os.chdir(mypath)
-onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
-cols = ""
-for e in onlyfiles:
-    workbook = xlrd.open_workbook(e)
-    worksheet = workbook.sheet_by_index(0)
-    for idx, cell_obj in enumerate(worksheet.row(0)):
-        cols =  cols + cell_obj.value + ","
-    print(cols)
+mypath = "/Users/fatosismali/Desktop/DataScience/MinistryOfData/datasets/Kosovo datasets/"
+
+
+def find_files(directory, pattern):
     cols = ""
+    for root, dirs, files in os.walk(directory):
+        for basename in files:
+            if fnmatch.fnmatch(basename, pattern):
+
+                filename = os.path.join(root, basename)
+
+                workbook = xlrd.open_workbook(filename)
+                worksheet = workbook.sheet_by_index(0)
+                for idx, cell_obj in enumerate(worksheet.row(0)):
+                    cols =  cols + str(cell_obj.value) + ","
+                print(cols)
+                cols = ""
+
+
+find_files(mypath, '*.xlsx')
